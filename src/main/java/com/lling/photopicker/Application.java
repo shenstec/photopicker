498e3095090d338592c9632261f7d4b61e302366
shenshilei <ssl_java@163.com>
2017-05-02 21:35:07 +0800

    第一次提交

---
+++ src/main/java/com/lling/photopicker/Application.java
@@ -0,0 +1,26 @@
+package com.lling.photopicker;
+
+/**
+ * Description :
+ * Author : lauren
+ * Email  : lauren.liuling@gmail.com
+ * Blog   : http://www.liuling123.com
+ * Date   : 2016/3/4
+ */
+public class Application extends android.app.Application {
+
+//    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        context = this.getApplicationContext();
+    }
+
+//    public static Context getContext() {
+//        return context;
+//    }
+
+
+
+}
